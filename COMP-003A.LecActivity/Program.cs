53c8e0e81cd1adfa6ceafd8bf09bfac1145e493d
jovanibenavides <jovanibenavides@LEM231-30>
2023-01-18 18:56:13 -0800

    Add project files.

---
+++ COMP-003A.LecActivity/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a single-line comment 
+/* This is a multi-line comment 
+ *  Author: Jovani Benavides
+ *  Course: Comp-003A
+ *  Purpose: Lecture activity for this week
+ */
+namespace COMP_003A.LecActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!\n"); // Add comments 
+            Console.WriteLine("this is a string\n");
+            Console.WriteLine("Name:Jovani Benavides\n");
+        }
+    }
+}
